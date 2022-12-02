3c8163b00858f8e8fcefda9b1ef587c674d6272d
MichaelVL <mvl.gh@network42.dk>
2022-12-02 18:05:49 +0100

    Do not assume a TTY for github action

--- scripts/chart-testing.sh
+++ scripts/chart-testing.sh
@@ -4,4 +4,4 @@ set -ex
 
 CHARTS="$(cd "$(dirname "$0")/.." && pwd)"
 
-docker run --rm -it -u $(id -u) --workdir=/data -v $CHARTS:/data:ro quay.io/helmpack/chart-testing:v3.7.1 ct lint --config scripts/configs/ct-lint.yaml
+docker run --rm -i -u $(id -u) --workdir=/data -v $CHARTS:/data:ro quay.io/helmpack/chart-testing:v3.7.1 ct lint --config scripts/configs/ct-lint.yaml
